90c9bb936955b87d1037e13f18549982c86e767d
rn4nv <dorjuan@ya.ru>
2023-02-13 06:15:02 +0700

    Wrote a program

--- Control_task/Program.cs
+++ Control_task/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] FirstArray = new string[4] { "hello", "2", "world", ":-)" };
+string[] SecondArray = new string[FirstArray.Length];
+
+void Result(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[count] = FirstArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Result(FirstArray, SecondArray);
+PrintArray(SecondArray);
